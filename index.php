ddf7f0d0ac46be8a38b6eaff0e307ba33780455a
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-01-19 14:00:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$faq = [
+    [
+        "question" => "" ,
+        "answer" => "",
+    ],
+    [
+        "question" => "",
+        "answer" => "",
+    ],
+    [
+        "question" => "",
+        "answer" => "",
+    ],
+    [
+        "question" => "",
+        "answer" => "",
+    ],
+    [
+        "question" => "",
+        "answer" => "",
+    ],
+]
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google faq</title>
+</head>
+<body>
+    
+</body>
+</html>
